e79c58897e45b8a8e3fd02adfd17d9a5fc32df47
SkyHawkB <danielmey626@gmail.com>
2018-04-18 18:28:02 -0400

    the rest of it

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const fs = require('fs');
+fs.writeFileSync('./logs/events.log', 'Server started!');
+const log = (message) => {
+  fs.appendFileSync('./logs/events.log', `${message}\n`);
+}
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
 http.listen(3000, () => {
-  console.log('Listening on *:3000!');
+  log('Listening on *:3000!');
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected!');
+  log('User connected!');
+  socket.broadcast.emit('A user has joined the chat!');
+
+  socket.on('disconnect', () => {
+    log('User disconnected!');
+    socket.broadcast.emit('A user has left the chat!');
+  });
+  socket.on('message', (message) => {
+    log(`Message sent: "${message}"!`);
+    io.emit('message', message);
+  });
 });
