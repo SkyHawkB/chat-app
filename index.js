969773510ee6e46a01e543b567fcdf3df0459583
SkyHawkB <danielmey626@gmail.com>
2018-05-13 15:30:35 -0400

    move stuff around

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
@@ -7,20 +8,16 @@ const log = (message) => {
   fs.appendFileSync('./logs/events.log', `${message}\n`);
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 http.listen(3000, '0.0.0.0', () => {
   log('Listening on *:3000!');
 });
 
 io.on('connection', (socket) => {
   log('User connected!');
-  socket.broadcast.emit('A user has joined the chat!');
 
   socket.on('disconnect', () => {
     log('User disconnected!');
-    socket.broadcast.emit('A user has left the chat!');
   });
   socket.on('message', (message) => {
     log(`Message sent: "${message}"!`);
