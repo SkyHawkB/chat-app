ea5c3bab7db0f7a1dc3d85d5029881c92d31bda1
SkyHawkB <danielmey626@gmail.com>
2018-06-06 20:06:32 -0400

    better logging!

--- index.js
+++ index.js
@@ -26,18 +26,34 @@ const sendDiscordMessage = (message) => {
 const fs = require('fs');
 const moment = require('moment');
 const colors = require('colors');
-
+const timestamp = () => {
+  return moment().format('MM-DD-YYYY HH:MM:SS') + ' > ';
+};
+const log = (message, file) => {
+  if(fs.existsSync(`./logs/${file}.log`)) {
+    fs.appendFileSync(`./logs/${file}.log`, timestamp() + message + '\n');
+  } else {
+    fs.writeFileSync(`./logs/${file}.log`, timestamp() + message + '\n');
+  }
+};
 
 app.use(express.static(__dirname + '/public'));
-http.listen(3000, '0.0.0.0', () => {});
+http.listen(3000, '0.0.0.0', () => {
+  log('Server Started!', 'events');
+});
 io.on('connection', (socket) => {
   socket.on('message', (message) => {
     io.emit('message', message);
+
+    log('Message sent!', 'events');
+    log(message, 'messages');
 
     sendDiscordMessage(message);
   });
   socket.on('join', (user) => {
     onlineUsers[user.id] = user.nick;
+
+    log(`"${user.nick}" joined the chat!`, 'events');
 
     io.emit('userUpdate', onlineUsers);
   });
@@ -46,6 +62,8 @@ io.on('connection', (socket) => {
 
     for(let key in keys) {
       if(!io.sockets.sockets.hasOwnProperty(key)) {
+        log(`"${onlineUsers[keys[key]]}" left the chat!`, 'events');
+
         delete onlineUsers[keys[key]];
         break;
       }
@@ -56,7 +74,7 @@ io.on('connection', (socket) => {
 });
 
 client.on('ready', () => {
-  console.log('Discord Integration Active!');
+  console.log(colors.green('Discord Integration Active!'));
 });
 client.on('message', (message) => {
   if(!message.guild || message.author.bot) return;
@@ -86,9 +104,9 @@ client.on('message', (message) => {
   }
 });
 client.on("error", (error) => {
-  const timestamp = moment().format('YYYY-MM-DD HH:MM:SS');
-  fs.writeFileSync(`./logs/${timestamp}.log`);
+  const timestamp = moment().format('YYYY-MM-DD HH-MM-SS');
+  fs.writeFileSync(`./logs/error/${timestamp}.log`);
   console.log(colors.red(`The program encountered an error!`));
-  console.log(colors.yellow(`More information can be found at "./logs/${timestamp}.log" !`));
+  console.log(colors.yellow(`More information can be found at "./logs/error/${timestamp}.log" !`));
 });
 client.login(botConfig.token);
